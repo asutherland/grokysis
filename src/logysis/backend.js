57241168b52267c02011cbb605c953781025a43d
Andrew Sutherland <asutherland@asutherland.org>
2017-12-04 17:51:01 -0500

    status commit to switch machines

---
+++ src/logysis/backend.js
@@ -0,0 +1,24 @@
+class BackendRouter() {
+  constructor() {
+    self.addEventListener("message", this._onMessage.bind(this));
+  }
+
+  _onMessage(evt) {
+    const data = evt.data;
+    const expectsReply = !!data.msgId;
+
+    const type = data.payload;
+    const handlerName = "msg_" + type;
+
+    try {
+      this[handlerName](data.payload);
+    } catch(ex) {
+      console.error(`Problem processing message of type ${type}:`, data, ex);
+    }
+  }
+
+  msg_init({ name }) {
+  }
+}
+
+const router = new BackendRouter();
---
+++ src/logysis/frontend.js
@@ -0,0 +1,50 @@
+import BackendWorker from 'worker-loader!./backend.js';
+
+class LogAnalysisFrontend {
+  /**
+   * The frontend name determines the root IndexedDB database name used.
+   * Per-session databases are created that are prefixed with this name.  You
+   * probably want to pick a single app-specific name and hardcode it.
+   */
+  constructor(name) {
+    this.name = name;
+    this._worker = new BackendWorker();
+    this._worker.addEventListener("message", this._onMessage.bind(this));
+
+    this._awaitingReplies = new Map();
+    this._nextMsgId = 1;
+
+    this._sendNoReply({
+      type: "init",
+      name
+    });
+  }
+
+  _onMessage(evt) {
+
+  }
+
+  _sendNoReply(payload) {
+    this._worker.postMessage({
+      msgId: 0,
+      payload
+    });
+  }
+
+  _sendAndAwaitReply(payload) {
+    this._worker.postMessage({
+      msgId: this._nextMsgId++,
+      payload
+    });
+  }
+
+  createSession() {
+
+  }
+
+  async loadSession(sessionName) {
+
+  }
+}
+
+export default CrashAnalysisFrontend;
