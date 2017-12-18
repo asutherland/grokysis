cf9c49194b660c27b7ccc74fab515e67d949f40d
Andrew Sutherland <asutherland@asutherland.org>
2017-12-18 15:22:48 -0500

    extract ParseDriver, provide ParseProgress feedback channel

---
+++ src/logysis/parse_progress.js
@@ -0,0 +1,9 @@
+/**
+ * Interface to report notable log parsing events and general progress from
+ * the parsing process in the back-end to the front-end.  For use for when
+ * the front-end potentially cares about what would otherwise be a console.log()
+ * call.
+ */
+export default class ParseProgress {
+  constructor()
+}
