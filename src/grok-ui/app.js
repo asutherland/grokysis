a83ea4a461e5c1f575da855a25d0aaec4b93ce67
Andrew Sutherland <asutherland@asutherland.org>
2018-03-06 15:09:04 -0500

    add splitter for artifact sidebar

--- src/grok-ui/app.js
+++ src/grok-ui/app.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {
+  ReflexContainer,
+  ReflexSplitter,
+  ReflexElement
+} from 'react-reflex';
+
 import NotebookContainer from '../notebook-ui/components/Container.jsx';
 
 import SearchFieldSheet from './components/sheets/search_field.jsx';
@@ -8,12 +14,17 @@ import SearchFieldSheet from './components/sheets/search_field.jsx';
 import GrokAnalysisFrontend from '../grokysis/frontend.js';
 
 import 'semantic-ui-css/semantic.min.css';
+import 'react-reflex/styles.css';
 
 import './grok-ui.css';
 import '../notebook-ui/notebook-ui.css';
 
 /**
- * The UI is a
+ * Two vertical panes with a splitter between them.  The left pane displays the
+ * notebook UI for exploration, the right pane displays the notebook UI for
+ * created diagrams/state machines/other intentional documentation artifacts.
+ * Since I also have portrait monitors, a horizontal pane configuration may
+ * eventually show up.
  */
 class GrokApp extends React.Component {
   constructor(props) {
@@ -23,7 +34,9 @@ class GrokApp extends React.Component {
 
     this.state = {
       grokCtx,
-      initialSheets: [
+      // XXX The initial sheets objects seem like they'd rather be part of a
+      // higher level session management system.  But this suffices for now.
+      initialExplorationSheets: [
         {
           labelWidget: 'Searchfox Search',
           awaitContent: null,
@@ -31,17 +44,44 @@ class GrokApp extends React.Component {
             return <SearchFieldSheet {...props} />;
           }
         }
+      ],
+      initialArtifactSheets: [
+        {
+          labelWidget: 'Diagram',
+          awaitContent: null,
+          contentFactory: (props) => {
+            return <div></div>;
+          }
+        }
       ]
     };
   }
 
   render() {
+    const explorationProps = {
+      grokCtx: this.state.grokCtx
+    };
+    const artifactProps = {
+      grokCtx: this.state.grokCtx
+    }
+
     return (
-      <NotebookContainer
-        passProps= { { grokCtx: this.state.grokCtx } }
-        grokCtx={ this.state.grokCtx }
-        initialSheets={ this.state.initialSheets }
-        />
+      <ReflexContainer className="grokApp" orientation="vertical">
+        <ReflexElement className="left-pane">
+          <NotebookContainer
+            passProps={ explorationProps }
+            initialSheets={ this.state.initialExplorationSheets }
+            />
+        </ReflexElement>
+        <ReflexSplitter />
+        <ReflexElement className="right-pane"
+          minSize="200" maxSize="800">
+          <NotebookContainer
+            passProps={ artifactProps }
+            initialSheets={ this.state.initialArtifactSheets }
+            />
+        </ReflexElement>
+      </ReflexContainer>
     );
   }
 }
